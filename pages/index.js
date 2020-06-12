14e31031937de8e299c1ea7522da775f66cb624a
azinmo <66830213+azinmo@users.noreply.github.com>
2020-06-12 16:49:53 +0430

    add files via upload

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import { useDispatch } from "react-redux";
+
+import Clock from "../components/clock";
+import Counter from "../components/counter";
+import { tick } from "../lib/slices/clockSlice";
+import useInterval from "../lib/useInterval";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { i18n, Link, withTranslation } from "../i18n";
+import Dashboard from "../components/dashboard";
+
+const IndexPage = ({ t }) => {
+  const dispatch = useDispatch();
+  // Tick the time every second
+  // useInterval(() => {
+  //dispatch(tick({ light: true, lastUpdate: Date.now() }));
+  //}, 1000);
+
+  return (
+    <>
+      {/* <h1>Next.js + PWA = {t("awesome").toLocaleUpperCase()}!</h1>
+      
+      <CssBaseline />
+      <Clock />
+      <Counter /> */}
+      <Dashboard />
+    </>
+  );
+};
+
+IndexPage.getInitialProps = async () => ({
+  namespacesRequired: ["common"],
+});
+
+export default withTranslation("common")(IndexPage);
